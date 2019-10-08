fdbb6e4fb3cd2bab1e2b3364236252a53ed038d8
Keerthi Mani <carkeane.getz@gmail.com>
2019-10-07 22:17:58 -0400

    Still images are animated by on click function

--- assets/javascript/API.js
+++ assets/javascript/API.js
@@ -37,27 +37,24 @@ $(document).ready(function () {
                 seaAnimalImages.attr("data-state", "still");
                 animalDiv.append(seaAnimalImages);
                 $("#animals").append(animalDiv);
-
-
-                // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-                $("#animals").on("click", ".waterAnimal-Images", function () {
-
-                    var state = $(this).attr("data-state");
-                    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                    // Then, set the image's data-state to animate
-                    if (state === "still") {
-                        $(this).attr("src", $(this).attr("data-animate"));
-                        $(this).attr("data-state", "animated");
-                    }
-                    else {
-                        $(this).attr("src", $(this).attr("data-still"));
-                        $(this).attr("data-state", "still");
-                    }
-                });
             }
         });
     }
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    $(document).on("click", ".waterAnimal-Images", function () {
 
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animated");
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 
     // Function for displaying water animal data
     function renderButtons() {
